cf8debb8c21c4f1a6b8067d4e022ba540fae4fee
Victor Aguilera <victoraguielrago@gmail.com>
2020-02-18 16:34:51 -0300

    draft

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
+import Sunburst from 'sunburst-chart'
+import data from './data.json'
 import './App.css';
 
 function App() {
+
+  useEffect(() => {
+    const myChart = Sunburst();
+    myChart.data(data)
+    .size('size')
+    .color('color')
+    (document.getElementById('chart'));
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="chart" />
     </div>
   );
 }
